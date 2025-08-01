eb1bb5564fdd569100c0754b0fd38866924ed63e
Bryan Guirola <bryanamilcarguirolamendezsgc@gmail.com>
2025-07-31 19:27:46 -0600

    Actualización de configuración providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (env('APP_ENV') === 'production') {
-            $url->forceScheme('https');
+            URL::forceScheme('https');
         }
     }
 }
