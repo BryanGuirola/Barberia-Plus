e3ffc828b4dd6d62cec2e37959f5b1bfbe9a9455
Bryan Guirola <bryanamilcarguirolamendezsgc@gmail.com>
2025-08-02 23:54:52 -0600

    Detalles finales

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
-#!/bin/sh
-# Espera a que la base de datos esté lista
 sleep 10
-
-# Corre migraciones (solo una vez por arranque)
 php artisan migrate --force
-
-# Inicia el servidor de Laravel
 php artisan serve --host=0.0.0.0 --port=$PORT
